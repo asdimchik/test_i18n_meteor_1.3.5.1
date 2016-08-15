0c78750216c984359f75e695cafde1106b40c71d
D.Krychylskyy <d.krychylskyy@gmail.com>
2016-08-15 10:49:14 +0200

    added new page for creating post and link

--- client/main.js
+++ client/main.js
@@ -1,3 +1,8 @@
 /**
  * Created by dkrichilsky on 08/08/2016.
  */
+Meteor.startup(function () {
+    Tracker.autorun(function () {
+        console.log('Tehere are ' + Posts.find().count() + ' posts');
+    });
+})
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,21 +1,21 @@
 /**
  * Created by dkrichilsky on 08/08/2016.
  */
-if (Posts.find().count() === 0) {
-    var i = 0;
-    while (i < 50) {
-        Posts.insert({
-            title: 'Introducing Telescope',
-            url: 'http://sachagreif.com/introducing-telescope/'
-        });
-        Posts.insert({
-            title: 'Meteor',
-            url: 'http://meteor.com'
-        });
-        Posts.insert({
-            title: 'The Meteor Book',
-            url: 'http://themeteorbook.com'
-        });
-        i++;
-    }
-}
+// if (Posts.find().count() === 0) {
+//     var i = 0;
+//     while (i < 50) {
+//         Posts.insert({
+//             title: 'Introducing Telescope',
+//             url: 'http://sachagreif.com/introducing-telescope/'
+//         });
+//         Posts.insert({
+//             title: 'Meteor',
+//             url: 'http://meteor.com'
+//         });
+//         Posts.insert({
+//             title: 'The Meteor Book',
+//             url: 'http://themeteorbook.com'
+//         });
+//         i++;
+//     }
+// }
